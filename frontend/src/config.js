b3fc4c9008f75821b91490136561232cbe3f972c
Uswah Alvi <uswaaalvi@gmail.com>
2026-09-22 03:03:36 +0500

    fix: use env for base urls

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000/api/v1";
-export const URL = "http://localhost:3000";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL 
+export const URL = import.meta.env.VITE_BASE_URL
